e17aec1e2494b6badd921af88738dccb6019b30a
Iliya Zhechev <i4ko95@gmail.com>
2016-12-15 03:08:07 +0200

    init

---
+++ public/config.js
@@ -0,0 +1,10 @@
+let room = {
+    broadcast: 'broadcast player',
+    gameUpdate: 'game update',
+    getPlayerId: 'new player id'
+};
+
+var module = module || { exports: {} };
+module.exports = {
+    room
+};
